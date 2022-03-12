8223f399b71bc30323931e87abf8657c1fbcf2d3
John Knott <john.knott@gmail.com>
2022-03-12 15:17:48 +0000

    Added changelog and license and intial github-actions for CI.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+I'll try to follow the format of: https://keepachangelog.com/en/1.0.0/
+
+## Unreleased
+
+## [0.0.1] - 2022-04-12
+
+### Added
+
+- Initial release.
+- Core functionality all integrated.
+- Added full test coverage.
+- Added credo.
+- Added coveralls.
--- mix.exs
+++ mix.exs
@@ -1,10 +1,27 @@
 defmodule DebounceAndThrottle.MixProject do
   use Mix.Project
+  @github_url "https://github.com/johnknott/debounce-and-throttle"
 
   def project do
     [
       app: :debounce_and_throttle,
-      version: "0.1.0",
+      version: "0.9.0",
+      name: "Debounce and Throttle",
+      description: "Provides simple debounce and throttle functionality",
+      source_url: @github_url,
+      homepage_url: @github_url,
+      files: ~w(mix.exs lib LICENSE.md README.md CHANGELOG.md),
+      package: [
+        maintainers: ["John Knott"],
+        licenses: ["MIT"],
+        links: %{
+          "GitHub" => @github_url
+        }
+      ],
+      docs: [
+        main: "readme",
+        extras: ["README.md", "CHANGELOG.md"]
+      ],
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,7 +41,8 @@ defmodule DebounceAndThrottle.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
